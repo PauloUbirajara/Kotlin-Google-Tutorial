6e7ee50f590d604ae32aef79f2a82e60d965e00a
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2022-01-13 10:59:08 -0300

    Adicionar evento para esconder teclado após apertar Enter

--- app/src/main/java/com/example/tipcalculatorimproved/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatorimproved/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipcalculatorimproved
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculatorimproved.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -16,6 +20,26 @@ class MainActivity : AppCompatActivity() {
         updateTipText()
         binding.calculateButton.setOnClickListener {
             updateTipText()
+        }
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean{
+        return when (keyCode) {
+            KeyEvent.KEYCODE_ENTER ->  {
+                hideKeyboard(view)
+                true
+            }
+            else -> {
+                false
+            }
         }
     }
 
