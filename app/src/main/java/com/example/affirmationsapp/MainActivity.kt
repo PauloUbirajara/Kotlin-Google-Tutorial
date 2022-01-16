966f67bf79f477e289f88d9ef5f9fb59b912761d
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2022-01-16 16:06:54 -0300

    Atribuir adapter criado ao RecyclerView na 'MainActivity'

--- app/src/main/java/com/example/affirmationsapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationsapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.affirmationsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmationsapp.adapter.ItemAdapter
+import com.example.affirmationsapp.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val dataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, dataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
